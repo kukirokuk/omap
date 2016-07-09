21807c6c3b6142b5c88a2b20ed69ae91987d30dc
Oleksandr Vinnichuk <oleksanr.v@gmail.com>
2016-07-09 23:18:53 +0300

    room model

--- map/models.py
+++ map/models.py
@@ -1,5 +1,24 @@
-from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    """Room Model"""
+
+    class Meta(object):
+        verbose_name = u"Rooms"
+        verbose_name_plural = u"Room"
+
+    room_name = models.CharField(
+        max_length=256,
+        blank=False,
+        verbose_name=u"Room name")
+
+    emp_number = models.CharField(
+        max_length=256,
+        blank= True,
+        verbose_name=u"Count of places")
+
+    def __unicode__(self):
+
+            return u"%s" % (self.room_name)
--- map/views.py
+++ map/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from models import Room
 
 # Create your views here.
 def map(request):
-    return render(request, 'map.html')
+    rooms = Room.objects.all()
+    room1, room2, room3, room4 = rooms[0], rooms[1], rooms[2], rooms[3]
+
+    return render(request, 'map.html', {'room1': room1, 'room2': room2,
+         'room3': room3, 'room4': room4})
